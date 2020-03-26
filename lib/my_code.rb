f3cf9f96fa4ed05d86fe3394f811ab75a0d4d13d
Lisa Jung <lisahjung@gmail.com>
2020-03-26 19:28:23 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -62,6 +62,19 @@ def reduce_to_all_true(source_array)
 end
 source_array
 end
+
+def reduce_to_any_true(source_array)
+  i = 0 
+  while i <source_array.length
+  if source_array[i]== true
+    return true
+  else
+    return false
+  end
+  i+=1 
+end
+source_array
+end
   
   
 
