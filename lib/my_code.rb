53abd8ea316284752f1a288407c1808b69032f73
Lisa Jung <lisahjung@gmail.com>
2020-03-26 19:28:06 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -49,6 +49,20 @@ def reduce_to_total(source_array, starting_point = 0)
 end
 total
 end
+
+def reduce_to_all_true(source_array)
+  i = 0 
+  while i <source_array.length
+  if !source_array[i]== true
+    return false
+  else
+    return true
+  end
+  i+=1 
+end
+source_array
+end
+  
   
 
 
